86a6d6d8fd7fa0f3b045c732a3b316c228c28b95
Yousef-Koriem <koriemyousef@gmail.com>
2021-10-05 00:19:56 +0200

    new edit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-
+function writeHTMLasJS(){
+document.write("    <nav>");
+document.write("      <h2>Yousef Koriem<\/h2>");
+document.write("      <div>");
+document.write("        <a href=\"..\/index.html\">Home<\/a>");
+document.write("        <a href=\"..\/about\/index.html\">About<\/a>");
+document.write("        <a href=\"\">Projects<\/a>");
+document.write("        <a href=\"..\/contact\/index.html\">Contact<\/a>");
+document.write("      <\/div>");
+document.write("      <div>");
+document.write("        <a href=\"https:\/\/qlink.to\/BlueFox39\">Login<\/a>");
+document.write("        <a href=\"https:\/\/yousefkoriem.wordpress.com\/\">WordPress<\/a>");
+document.write("      <\/div>");
+document.write("    <\/nav>");
+}
+writeHTMLasJS();
